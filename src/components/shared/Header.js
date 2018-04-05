bc746a68c94920c85da01e0e0e00175a85482aa2
The Tien <tiendt2311@gmail.com>
2018-04-05 16:42:47 +0700

    custom navbar when login

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,30 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Link, NavLink } from 'react-router-dom';
 
 class Header extends Component {
+  static propTypes = {
+    users: PropTypes.object.isRequired
+  }
+
   render(){
+    const { isAuthenticate, currentUser } = this.props.users;
+
+    const userLinks = (
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02" >
+        <ul className="navbar-nav mt-2 mr-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/vouchers/new">Create Voucher <span className="sr-only">(current)</span></NavLink>
+          </li>
+        </ul>
+        <ul className='navbar-nav mt-2'>
+          <li className="nav-item pull-right">
+            <a className="nav-link pull-right" to="/login">Log Out</a>
+          </li>
+        </ul>
+      </div>
+    );
+    const guestLinks = (
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02" >
+        <ul className='navbar-nav ml-auto mt-2'>
+          <li className="nav-item pull-right">
+            <NavLink className="nav-link pull-right" to="/login">Login</NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+
     return(
-      <nav className="navbar navbar-expand-lg navbar-inverse navbar-fixed-top navbar-dark bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-fixed-top navbar-dark bg-primary">
         <NavLink className="navbar-brand" to="/">Voucher Trader</NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul className="navbar-nav mt-2 mr-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/vouchers/new">Create Voucher <span className="sr-only">(current)</span></NavLink>
-            </li>
-          </ul>
-          <ul className='navbar-nav mt-2'>
-            <li className="nav-item pull-right">
-              <NavLink className="nav-link pull-right" to="/login">Login</NavLink>
-            </li>
-          </ul>
-        </div>
+        {isAuthenticate ? userLinks : guestLinks}
       </nav>
     );
   }
 }
 
-export default Header
+const mapStateToProps = (state) => ({
+  users: state.users
+})
+
+export default connect(mapStateToProps)(Header);
