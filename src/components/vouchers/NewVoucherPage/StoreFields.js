8adfc2e7451a9bb439d51b70265fd85b191a7ea5
The Tien <tiendt2311@gmail.com>
2018-04-13 13:10:15 +0700

    added step function'

--- src/components/vouchers/NewVoucherPage/StoreFields.js
+++ src/components/vouchers/NewVoucherPage/StoreFields.js
@@ -4,36 +4,22 @@ import TextFieldGroup from '../../shared/TextFieldGroup';
 import PlacesWithStandaloneSearchBox from '../../shared/PlacesWithStandaloneSearchBox';
 
 class StoreFields extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      name: '',
-      address: ''
-    }
-  }
-
-  handleChange = (e) =>{
-    console.log(e);
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
   render() {
     return (
       <div>
-        <h5 className="mb-3">Store Informations</h5>
+        <h4 className="mt-3 text-center font-weight-bold">Store Informations</h4>
         <form className="ml-2">
           <TextFieldGroup 
             name='name'
-            value={this.state.name}
-            handleChange={this.handleChange}
+            value={this.props.fields.name}
+            handleChange={this.props.handleChange}
           />
           <PlacesWithStandaloneSearchBox
             googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
             name='address'
             loadingElement={<div style={{ height: `100%` }}/>}
-            value={this.state.address}
-            handleChange={this.handleChange}
+            value={this.props.fields.address}
+            handleChange={this.props.handleChange}
           />
 
         </form>
--- src/components/vouchers/NewVoucherPage/index.js
+++ src/components/vouchers/NewVoucherPage/index.js
@@ -4,22 +4,87 @@ import NewVoucherForm from './NewVoucherForm';
 import StoreFields from './StoreFields';
 import VoucherInfoFields from './VoucherInfoFields';
 import VoucherPostingOption from './VoucherPostingOption';
+import { 
+  Route,
+  Switch
+} from 'react-router-dom';
 import {createVoucher} from '../../../actions/voucher';
 import { addFlashMessage } from '../../../actions/message';
 
 class NewVoucherPage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      store: {
+        name: '',
+        address: ''
+      },
+
+      voucher: {
+        type: '',
+        description: '',
+        price: '',
+        code: '',
+        address_receiver: ''
+      }, 
+      currentStep: 1
+    }
+  }
+
+  handleVoucherFieldsChange = (e) =>{
+    this.setState({
+      voucher: {
+        [e.target.name]: e.target.value
+      }
+    });
+  }
+
+  changeStep = (step) => {
+    this.setState({
+      currentStep: this.state.currentStep + step
+    })
+  }
+
+  handleStoreFieldsChange = e =>{
+    this.setState({
+      store: {
+        [e.target.name]: e.target.value 
+      }
+    });
+  }
+
+  showStep = () => {
+    switch(this.state.currentStep){
+      case 1:
+        return(
+          <StoreFields
+            fields={this.state.store}
+            handleChange={this.handleStoreFieldsChange} 
+          />
+        )
+      case 2:
+        return(
+          <VoucherInfoFields
+            fields={this.state.voucher}
+            handleChange={this.handleStoreFieldsChange} 
+          />
+        )
+      case 3:
+        return(
+          <VoucherPostingOption
+            fields={this.state} 
+          />
+        )
+    }
+  }
   render(){
     return(
       <div className='row'>
         <div className='col-md-4 offset-md-4'>
-          <h4 className="text-center m-2">Create voucher</h4>
-
-          <StoreFields />
-          <VoucherInfoFields />
-          <VoucherPostingOption />
+          {this.showStep()}
         </div>
       </div> 
-    );
+    );      
   }
 }
 
