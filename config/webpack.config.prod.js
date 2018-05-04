5b7da6fc236ca460e203ac295fe673d99793f9be
The Tien <tiendt2311@gmail.com>
2018-05-05 05:16:59 +0700

    added scss loader

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -166,7 +166,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.scss$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -177,6 +177,9 @@ module.exports = {
                     },
                   },
                   use: [
+                    {
+                      loader: require.resolve('sass-loader'),
+                    },
                     {
                       loader: require.resolve('css-loader'),
                       options: {
