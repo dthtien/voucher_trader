cf102d40876d040ac72f66b5e9b8eb7630e2bd8f
The Tien <tiendt2311@gmail.com>
2018-05-23 22:33:46 +0700

    show cart on shipping

--- src/components/ShippingPage/index.js
+++ src/components/ShippingPage/index.js
@@ -1,15 +1,64 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import ShippingForm from './ShippingForm';
+import { fetchCart } from '../../actions/cart';
+import CartItem from '../Cart/CartItem';
+import { toast } from 'react-toastify';
+import Spinner from '../shared/Spinner';
+import isEmpty from 'lodash/isEmpty';
 
 class ShippingPage extends Component {
-
+  componentDidMount(){
+    if (this.props.isAuthenticate) {
+      this.props.fetchCart();
+    } else {
+      toast.error("Bạn phải đăng nhập trước");
+      this.props.history.push('/login');
+    }
+  }
+  renderCartItem = () =>{
+    const {list_cart_item} = this.props
+    if (list_cart_item.length > 0 ) {
+      return list_cart_item.map((cart, index) =>{
+        return (
+          <CartItem 
+            key={"cart-item" + index}
+            price={cart.price || 0}
+            quantity={cart.quantity || 0}
+            name={cart.voucher.name || ''}
+            images={!isEmpty(cart.voucher.images) ? cart.voucher.images[0] : {}}
+          />
+        );
+      });
+    } else {
+      return <Spinner />
+    }
+  }
   render() {
+    console.log("props",this.props.list_cart_item);
     return (
       <div className='container mt-5'>
-        <ShippingForm />
-      </div>  
+        <div className="row">
+          <div className="col-sm-12 col-md-8">
+            <div className="cart-overview js-cart">
+              <ul className="cart-items">
+                {this.renderCartItem()}
+              </ul>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <ShippingForm />
+          </div>
+        </div>
+      </div>   
     );
   }
 }
 
-export default ShippingPage;
+const mapStateToProps = (state) => ({
+  user: state.users.currentUser,
+  isAuthenticate: state.users.isAuthenticate,
+  list_cart_item: state.cart.list_cart_item
+})
+
+export default connect(mapStateToProps, {fetchCart})(ShippingPage);
